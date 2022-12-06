e21b48be21d9b83786806e700ef4e8730441918c
oscarbunting <you@example.com>
2022-12-06 17:42:24 +0000

    Added a history cookie to remebr history on refresh added favicon

--- script.js
+++ script.js
@@ -1,72 +1,73 @@
-// Get the input field, button, and result element
 const input = document.getElementById('names');
 const button = document.getElementById('calculate-button');
 const resultElement = document.getElementById('result');
 
-// Add an event listener to the button that
-// runs the calculate function when clicked
 button.addEventListener('click', calculate);
 
-// A variable to store the previous result
 let previousResult;
-
-// An array to store the names that have been called
 const called = [];
 
-// The calculate function gets the names from the input
-// field, calculates who gets the joint next, and
-// updates the page with the result
 function calculate() {
-  // Get the names from the input field
   const names = input.value.split(',');
-
-  // Use a random algorithm to calculate who gets the joint next
-  // Call the random function and store the result in a variable
   let result = random(names);
 
-  // Check if the result is the same as the previous result
-  // or if it has already been called
-  // If it is, call the random function again until a different result is returned
   while (result === previousResult || called.includes(result)) {
     result = random(names);
   }
 
-  // Update the page with the result
   resultElement.innerHTML = `Next up: ${result}`;
   resultElement.classList.add('highlight');
-
-  // Add the result to the history
   addToHistory(result);
-
-  // Store the result in a variable for the next time the function is called
   previousResult = result;
   
-  // Add the result to the array of called names
   called.push(result);
   
-  // If all names have been called, reset the called array
   if (called.length === names.length) {
     called.length = 0;
   }
+  
+  // Save the history to a cookie
+  saveHistoryToCookie();
 }
 
-// The addToHistory function adds the result to the history
-// list at the bottom of the page
 function addToHistory(result) {
-  // Create a new list item with the result
   const item = document.createElement('li');
   item.innerHTML = result;
 
-  // Add the item to the history list
   const history = document.getElementById('history');
   history.appendChild(item);
 }
 
-// The random function takes an array of names and
-// returns a random name from the array
 function random(names) {
-  // Generate a random index based on the length of the array
   const index = Math.floor(Math.random() * names.length);
-  // Return the name at the random index
   return names[index];
 }
+
+// Function to save the history to a cookie
+function saveHistoryToCookie() {
+  // Get the history list
+  const historyList = document.getElementById('history');
+  // Convert the history list to a string
+  const historyString = historyList.innerHTML;
+  // Create a date object to set the expiration date of the cookie
+  const date = new Date();
+  // Set the expiration date to one year from now
+  date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+  // Set the cookie with the history string and expiration date
+  document.cookie = `history=${historyString}; expires=${date.toUTCString()}`;
+}
+
+// Function to load the history from a cookie
+function loadHistoryFromCookie() {
+  // Get the history cookie
+  const historyCookie = document.cookie.replace(/(?:(?:^|.*;\s*)history\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  // If the cookie exists, update the history list with the saved history
+  if (historyCookie) {
+    document.getElementById('history').innerHTML = historyCookie;
+  }
+}
+
+// Call the loadHistoryFromCookie function when the page loads
+window.onload = function() {
+  loadHistoryFromCookie();
+};
